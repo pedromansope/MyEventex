4a497ab234ef57836809ce82fbfc2a330162128e
Pedro <pedrolucasmppe@gmail.com>
2022-01-19 15:00:45 -0300

    Subs send email

--- eventex/settings.py
+++ eventex/settings.py
@@ -30,6 +30,20 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
+#SENDING E-MAIL:
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+
+EMAIL_HOST = config('EMAIL_HOST')
+
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+EMAIL_PORT = config('EMAIL_PORT')
+
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 # Application definition
 
 INSTALLED_APPS = [
