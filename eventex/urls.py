c94e8b563ed489c706f24a6bcbb3b9d66f663d57
Pedro <pedrolucasmppe@gmail.com>
2022-02-02 16:26:02 -0300

    Adiciona pagina de agradecimento

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from subscriptions.views import subscribe
+from subscriptions.views import subscribe, detail
 import core.views
 
 urlpatterns = [
     path('', core.views.home),
     path('inscricao/', subscribe),
+    path('inscricao/<int:pk>/', detail),
     path('admin/', admin.site.urls),
 ]
